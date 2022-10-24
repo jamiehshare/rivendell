c2e796ee8139af05b814fcd7be2baef4c5c9fa50
jamiehshare <jamie.hudson@sharecreative.com>
2022-10-24 19:32:24 +0100

    removed_defunt_functions

--- R/clean_hf_emotion.R
+++ R/clean_hf_emotion.R
@@ -7,6 +7,7 @@
 #' @importFrom dplyr case_when
 #' @importFrom dplyr select
 #' @importFrom dplyr %>%
+#' @importFrom rlang .data
 #' @export
 #'
 
@@ -14,16 +15,16 @@ extract_emotion_distilbert <- function(df) {
 
   df %>%
     janitor::clean_names() %>%
-    dplyr::mutate(document = row_number()) %>%
-    tidyr::pivot_longer(cols = -document) %>%
-    dplyr::filter(!is.na(value)) %>%
-    tidyr::separate(value, into = c("entity_type","entity_score"), sep = ",")%>%
-    dplyr::mutate(entity_type = stringr::str_remove(entity_type, "\\{'label': '"),
-                  entity_type = stringr::str_remove(entity_type, "'"),
-                  entity_score = readr::parse_number(entity_score)) %>%
-    dplyr::select(-name) %>%
-    dplyr::group_by(document) %>%
-    dplyr::arrange(entity_type, .by_group = TRUE) %>%
-    dplyr::pivot_wider(names_from = entity_type, values_from = entity_score)
+    dplyr::mutate(document = dplyr::row_number()) %>%
+    tidyr::pivot_longer(cols = -.data$document) %>%
+    dplyr::filter(!is.na(.data$value)) %>%
+    tidyr::separate(.data$value, into = c("entity_type","entity_score"), sep = ",")%>%
+    dplyr::mutate(entity_type = stringr::str_remove(.data$entity_type, "\\{'label': '"),
+                  entity_type = stringr::str_remove(.data$entity_type, "'"),
+                  entity_score = readr::parse_number(.data$entity_score)) %>%
+    dplyr::select(-.data$name) %>%
+    dplyr::group_by(.data$document) %>%
+    dplyr::arrange(.data$entity_type, .by_group = TRUE) %>%
+    tidyr::pivot_wider(names_from = .data$entity_type, values_from = .data$entity_score)
 
 }
