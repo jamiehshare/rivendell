64df0b1ed94a4071a2be0740fd857bc4ecffd0be
jamiehshare <jamie.hudson@sharecreative.com>
2023-05-17 17:09:13 +0100

    Added Peaks and Pits functions + data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Peaks and Pits products regex patterns
+#'
+#' @format ## `product_entity`
+#' A data frame of different regex patterns for Peaks and Pits removal of products
+#' \describe
+#'   ...
+#' }
+#' @source <
+"product_entity"
+
+
+#' Peaks and Pits brand regex patterns
+#'
+#' @format ## `brand_entity`
+#' A data frame of different regex patterns for Peaks and Pits removal of brands
+#' \describe
+#'   ...
+#' }
+#' @source <
+"brand_entity"
---
+++ R/peaks_pits.R
@@ -0,0 +1,46 @@
+#' pp_products
+#'
+#' @param df input dataframe
+#' @param text_var column with posts
+#'
+#' @return Data Frame or Tibble object with text variable edited inline
+#' @export
+#'
+#' @examples
+#'
+pp_products <- function(df, text_var){
+
+  entities <- LimpiaR::entities
+
+  products <- rivendell::product_entity
+
+  strings <- entities$token
+  strings_2 <- products$pattern
+  strings <- c(strings, strings_2)
+  strings <- paste0(strings, collapse = "|")
+  replacement <- "product"
+
+  df %>%
+    dplyr::mutate({{text_var}} := stringr::str_replace_all(tolower( {{text_var}} ), strings, replacement))
+}
+
+
+#' pp_brands
+#'
+#' @param df Input dataframe
+#' @param text_var Column with posts
+#'
+#' @return Data Frame or Tibble object with text variable edited inline
+#' @export
+#'
+#' @examples
+#'
+pp_brands <- function(df, text_var){
+
+  brands <- rivendell::brand_entity
+  brands <- paste0(brands, collapse = "|")
+  replacement <- "brand"
+
+  df %>%
+    dplyr::mutate({{text_var}} := stringr::str_replace_all(tolower( {{text_var}} ), brands, replacement))
+}
